026a202f3cd9ab2ffc19ae8191b9d2d41cc86fc1
shaheryar Haider <shaheryarjerry@gmail.com>
2022-01-30 19:34:56 +0500

    check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
-import NavBar from './components/NavBar'
-import Welcome from './components/Welcome'
-import Services from './components/Services'
-import Transactions from './components/Transactions'
-import Footer from './components/Footer'
-const  App=()=> {
+import NavBar from "./components/NavBar";
+import Welcome from "./components/Welcome";
+import Services from "./components/Services";
+import Transactions from "./components/Transactions";
+import Footer from "./components/Footer";
+const App = () => {
   return (
-    <div className='min-h-screen'>
-    <div className='gradient-bg-welcome'> 
- <NavBar />   
- <Welcome />
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <NavBar />
+        <Welcome />
       </div>
       <Services />
       <Transactions />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const NavBar = () => {
-  return <div>
-    <h1>Navbar</h1>
+  return <div style={{height:'200px',alignItems:'center'}}>
+    <h1 style={{color:'white'}}>Navbar</h1>
+    <h1 style={{color:'white'}}>Navbar</h1>
+    <h1 style={{color:'white'}}>Navbar</h1>
   </div>;
 };
 
