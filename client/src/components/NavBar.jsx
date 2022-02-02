1ce4e60395ba04c13e581a3086fb657fb5a85ec4
shaheryar Haider <shaheryarjerry@gmail.com>
2022-02-02 20:46:32 +0500

    working on navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {HiMenuAlt4} from 'react-icons/hi'
+import {AiOutlineClose} from 'react-icons/ai'
+import logo from '../images/logo.png'
 
 const NavBar = () => {
-  return <div style={{height:'200px',alignItems:'center'}}>
-    <h1 style={{color:'white'}}>Navbar</h1>
-    <h1 style={{color:'white'}}>Navbar</h1>
-    <h1 style={{color:'white'}}>Navbar</h1>
-  </div>;
+  return <nav className='w-full flex md:justify-center justify-between items-center p-4'>
+<div className='md:flex-[0.5] flex-initial'>
+  <img src={logo} alt='logo' className='w-32 cursor-pointer'/>
+</div>
+  </nav>
 };
 
 export default NavBar;
